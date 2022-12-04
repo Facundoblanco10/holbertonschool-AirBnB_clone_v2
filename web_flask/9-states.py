96d9ce29fbc29fcbdccc9576f7c1a88f635c17c7
Facub10 <facub1011@gmail.com>
2022-12-04 02:59:06 +0000

    task 10

---
+++ web_flask/9-states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" script that starts a Flask web application"""
+
+from flask import Flask
+from flask import render_template
+from flask import request
+from models import storage
+
+app = Flask(__name__)
+
+
+    
+@app.route('/states', strict_slashes=False)
+def states_page():
+    from models.state import State
+    states = storage.all(State).values()
+    return render_template('9-states.html', states=states)
+
+@app.route('/states/<id>', strict_slashes=False)
+def states_id_page(id):
+    from models.state import State
+    states = storage.all(State).values()
+    state = None
+    for i in states:
+        if id == i.id:
+            state = i
+            break
+    return render_template('9-states.html', state=state, id=id)
+
+@app.teardown_appcontext
+def remove(exception):
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
